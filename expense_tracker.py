cd9956494435bc452ef669f2094e48b7d8ae4c7f
ChristianTStu <christiantstu@gmail.com>
2024-02-19 16:44:17 -0600

    Complete app layout

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,5 +1,24 @@
 def main():
-    pass
+    print('Running Expense Tracker')
+
+    # Get user input for expense.
+    get_user_expense()
+
+    # Write their expense to a file.
+    save_expense_to_file()
+
+    # Read file and summarize expenses.
+    summarize_expense()
+
+
+def get_user_expense():
+    print('Get User Expense')
+
+def save_expense_to_file():
+    print('Save Expense to File')
+    
+def summarize_expense():
+    print('Summarize Expense')
 
 
 if __name__ == '__main__':
