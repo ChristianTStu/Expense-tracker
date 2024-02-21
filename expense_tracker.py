f4e70cec557b0fb30cec52b9c56d050d1f575bc8
ChristianTStu <christiantstu@gmail.com>
2024-02-20 23:53:34 -0600

    Complete summarize_expenses func, Add color

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,18 +1,21 @@
+import calendar
+import datetime
 from expense import Expense
 
 
 def main():
     print('Running Expense Tracker')
     expense_file_path = 'expenses.csv'
+    budget = 2000
 
     # Get user input for expense.
-    #expense = get_user_expense()
+    expense = get_user_expense()
 
     # Write their expense to a file.
-    #save_expense_to_file(expense, expense_file_path)
+    save_expense_to_file(expense, expense_file_path)
 
     # Read file and summarize expenses.
-    summarize_expenses(expense_file_path)
+    summarize_expenses(expense_file_path, budget)
 
 
 def get_user_expense():
@@ -55,8 +58,9 @@ def save_expense_to_file(expense: Expense, expense_file_path):
     with open(expense_file_path, 'a') as f:
         f.write(f'{expense.name},{expense.amount},{expense.category}\n')
 
-def summarize_expenses(expense_file_path):
+def summarize_expenses(expense_file_path, budget):
     print('Summarize Expense')
+    print('-------------------------------------------------')
     expenses: list[Expense] = []
     with open(expense_file_path, 'r') as f:
         lines = f.readlines()
@@ -80,6 +84,27 @@ def summarize_expenses(expense_file_path):
     print('Expenses By Category 📈:')
     for key, amount in amount_by_category.items():
         print(f' {key}: ${amount:.2f}')
+
+    print('-------------------------------------------------')
+
+    total_spent = sum([x.amount for x in expenses])
+    print('💵 Total Spent:' + red(f' ${total_spent:.2f}'))
+
+    remaining_budget = budget - total_spent
+    print(f"💰 Budget Remaining: ${remaining_budget:.2f}")
+
+    now = datetime.datetime.now()
+    days_in_month = calendar.monthrange(now.year, now.month)[1]
+    remaining_days = days_in_month - now.day
+
+    daily_budget = remaining_budget / remaining_days
+    print('👉 Budget Per Day:' + green(f' ${daily_budget:.2f}'))
+
+def green(text):
+    return f"\033[92m{text}\033[0m"
+
+def red(text):
+    return f"\033[31m{text}\033[0m"
 
 
 if __name__ == '__main__':
