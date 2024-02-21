97f7d3e3b35ac279d8ff138f3a895413d5ec07f7
ChristianTStu <christiantstu@gmail.com>
2024-02-20 19:26:46 -0600

    Complete get_user_expense func

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,8 +1,12 @@
+from expense import Expense
+
+
 def main():
     print('Running Expense Tracker')
 
     # Get user input for expense.
-    get_user_expense()
+    expense = get_user_expense()
+    print(expense)
 
     # Write their expense to a file.
     save_expense_to_file()
@@ -32,8 +36,17 @@ def get_user_expense():
             print(f'{i + 1}. {category_name}')
 
         value_range = (f'[1 - {len(expense_categories)}]')
-        selected_index = input(f'Enter a category number {value_range}: ')
-        break
+        selected_index = int(input(f'Enter a category number {value_range}: ')) - 1
+
+        if selected_index in range(len(expense_categories)):
+            selected_category = expense_categories[selected_index]
+            new_expense = Expense(
+                name=expense_name, category= selected_category, amount= expense_amount
+            )
+            return new_expense
+        else:
+            print('Invaild category. Please try again!')
+
 
 def save_expense_to_file():
     print('Save Expense to File')
